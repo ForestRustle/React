f1bc4cc0aca2fae9dc45a12beacdd8b9ef440950
ForestRustle <fenix66690@mail.ru>
2025-08-24 16:07:16 +0300

    fix/ исправленно название в импорте

--- 1-start/src/components/Header/Header.jsx
+++ 1-start/src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import styes from './Header.module.css';
+import styles from './Header.module.css';
 import cs from 'classnames';
 
 function Header() {
   return (
-    <header className={cs(styes.header)}>
+    <header className={cs(styles.header)}>
       <img src="./bookmark.svg" alt="Логотип закладок" />
-      <div className={cs(styes.header_links)}>
+      <div className={cs(styles.header_links)}>
         <a href="#">Поиск фильмов</a>
         <a href="#">Мои фильмы</a>
-        <div className={cs(styes.login)}>
+        <div className={cs(styles.login)}>
           <a href="#">
             Войти
             <img src="./login.svg" alt="Иконка логина" />
